050d28db22d397a8dddcd9e4befacc60bc24060b
DESKTOP-UCP5P1F\caffeine111 <kaminari8`icloud.com>
2023-06-02 02:44:02 +0900

    ネガポジ変換のコードの第一完成版

--- negative-switch.cpp
+++ negative-switch.cpp
@@ -12,38 +12,38 @@ using namespace cv;
 
         Mat img_src = imread(file_src, 0);
         Mat img_dst;
-        if (img_src.empty()) {
-            cout << "Error: Failed to read the image" << endl;
+        if (!img_src.data) {
+            cout << "error" << endl;
             return -1;
-        }
+        }//入力画像の読み込みの確認
 
+        //ウィンドウの生成
         namedWindow(win_src, WINDOW_AUTOSIZE);
         namedWindow(win_hst, WINDOW_AUTOSIZE);
 
-        // ヒストグラム表示用、256 * 100 ピクセル、　0(黒) で
+        //ヒストグラム表示用の画像の宣言
         Mat img_hst = Mat::zeros(100, 256, CV_8UC1);
 
-        //必要な処理
+        //ネガポジ反転
         img_src.convertTo(img_dst, img_src.type(), -1.0, 255.0);
 
-       
+        //ヒストグラムの描画
         const int hdims[] = { 256 };
         const float hranges[] = { 0, 256 };
         const float* ranges[] = { hranges };
-        // 1チャンネル画像の度数分布表を計算
+        /
         Mat hist;
         calcHist(&img_src, 1, 0, Mat(), hist, 1, hdims, ranges);
-        // 度数の最大値を取得
+        
         double hist_min, hist_max;
         minMaxLoc(hist, &hist_min, &hist_max);
         
-        // ヒスとグラムを白線で描画
         for (int i = 0; i <= 255; i++) {
             int v = saturate_cast<int>(hist.at<float>(i));
             line(img_hst, Point(i, img_hst.rows), Point(i, img_hst.rows - img_hst.rows * (v / hist_max)), Scalar(255, 255, 255));
         }
         
-
+        //画像の表示と保存
         imshow(win_src, img_dst);
         imshow(win_hst, img_hst);
         imwrite( file_dst , img_hst );
