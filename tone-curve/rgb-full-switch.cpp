6548320b9f86414bdb87261fd962b52ebcac91c9
DESKTOP-UCP5P1F\caffeine111 <kaminari8`icloud.com>
2023-06-12 05:35:39 +0900

    RGB値入れ替えの第二完成版のアップロード

--- tone-curve/rgb-full-switch.cpp
+++ tone-curve/rgb-full-switch.cpp
@@ -6,22 +6,27 @@ using namespace cv;
 
 
 int main() {
+    //ウィンドウ名の宣言
     string win_src = "src";
     string win_dst = "dst";
     string win_dst2 = "dst2";
     string win_dst3 = "dst3";
     string win_dst4 = "dst4";
-    string win_dst5 = "dst5";    
+    string win_dst5 = "dst5";
+    //入力画像のpathの格納
     string file_src = "C:\\Users\\caffeine111\\Documents\\Falcon.jpg";
-   
-    Mat img_src = imread(file_src, 1);//入力画像の宣言と読み込み
 
-    Mat img_dst, img_dst2, img_dst3, img_dst4, img_dst5;//出力画像の宣言
+    //入力画像オブジェクトの宣言と読み込み
+    Mat img_src = imread(file_src, 1);
 
+    //出力画像オブジェクトの宣言
+    Mat img_dst, img_dst2, img_dst3, img_dst4, img_dst5;
+
+    //入力画像の読み込みの確認
     if (!img_src.data) {
         cout << "error" << endl;
         return -1;
-    }//入力画像の読み込みの確認
+    }
 
     // ウィンドウの生成
     namedWindow(win_src, WINDOW_AUTOSIZE);
@@ -30,7 +35,7 @@ int main() {
     namedWindow(win_dst3, WINDOW_AUTOSIZE);
     namedWindow(win_dst4, WINDOW_AUTOSIZE);
     namedWindow(win_dst5, WINDOW_AUTOSIZE);
-    
+
     //RGB値の編集
     vector<Mat> img_bgr(3);
     split(img_src, img_bgr);
@@ -47,16 +52,14 @@ int main() {
     imshow(win_dst3, img_dst3);
     imshow(win_dst4, img_dst4);
     imshow(win_dst5, img_dst5);
-    
+
     //画像の保存
-    imwrite("C:\\Users\\caffeine111\\Desktop\\my-opencv\\rgb-switch1.jpg", img_dst);
-    imwrite("C:\\Users\\caffeine111\\Desktop\\my-opencv\\rgb-switch2.jpg", img_dst2);
-    imwrite("C:\\Users\\caffeine111\\Desktop\\my-opencv\\rgb-switch3.jpg", img_dst3);
-    imwrite("C:\\Users\\caffeine111\\Desktop\\my-opencv\\rgb-switch4.jpg", img_dst4);
-    imwrite("C:\\Users\\caffeine111\\Desktop\\my-opencv\\rgb-switch5.jpg", img_dst5);
+    //imwrite("C:\\Users\\caffeine111\\Desktop\\my-opencv\\rgb-switch1.jpg", img_dst);
+    //imwrite("C:\\Users\\caffeine111\\Desktop\\my-opencv\\rgb-switch2.jpg", img_dst2);
+    //imwrite("C:\\Users\\caffeine111\\Desktop\\my-opencv\\rgb-switch3.jpg", img_dst3);
+    //imwrite("C:\\Users\\caffeine111\\Desktop\\my-opencv\\rgb-switch4.jpg", img_dst4);
+    //imwrite("C:\\Users\\caffeine111\\Desktop\\my-opencv\\rgb-switch5.jpg", img_dst5);
 
     waitKey(0);//キー入力待ち
     return 0;
 }
-
-
