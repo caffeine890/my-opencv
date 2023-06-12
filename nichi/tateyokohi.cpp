025c25a7d23943692bb03324de3b6ac8474d3949
DESKTOP-UCP5P1F\caffeine111 <kaminari8`icloud.com>
2023-06-12 17:37:03 +0900

    縦横比の第二完成版のアップロード

--- nichi/tateyokohi.cpp
+++ nichi/tateyokohi.cpp
@@ -6,27 +6,37 @@ using namespace std;
 using namespace cv;
 
 int main() {
+    //ウィンドウ名の宣言
     string win_src = "src";
     string win_dst1 = "dst1";
-    string file_src = "C:\\Users\\caffeine111\\Documents\\Falconikichi.jpg";
-    string file_dst = "C:\\Users\\caffeine111\\Documents\\Falconikichi2.jpg";
 
+     // 入力画像のpathの格納
+    string file_src = "C:\\Users\\caffeine111\\Documents\\Falconikichi.jpg";
+    //string file_dst = "C:\\Users\\caffeine111\\Documents\\Falconikichi2.jpg";
+    
+    //入力画像オブジェクトの宣言と読み込み
     Mat img_src = imread(file_src, 0);
+
+    //出力画像オブジェクトの宣言
     Mat img_dst1 = img_src.clone();
+
+    //入力画像の読み込みの確認
     if (!img_src.data) {
         cout << "error" << endl;
         return -1;
     }
-
+   
+    //縦横比の処理
     Rect rect = boundingRect(img_src);
     double aspectRatio = (double)(rect.height) / rect.width;
     cout << "Aspect Ratio: " << aspectRatio << endl;
     rectangle(img_dst1, rect, Scalar(128));
 
+    //ウィンドウの生成
     namedWindow(win_src, WINDOW_AUTOSIZE);
     namedWindow(win_dst1, WINDOW_AUTOSIZE);
     
-
+    //画像の表示
     imshow(win_src, img_src);
     imshow(win_dst1, img_dst1);
     
